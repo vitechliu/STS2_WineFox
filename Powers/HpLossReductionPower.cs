8483a0f897c184a213f44707b8ff545a428c2cbd
LuoTianOrange <2977277015@qq.com>
2026-05-17 05:44:29 +0800

    fix: 修复附魔金苹果的能力图标缺失的问题

--- Powers/HpLossReductionPower.cs
+++ Powers/HpLossReductionPower.cs
@@ -7,15 +7,18 @@ using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
 
 namespace STS2_WineFox.Powers
 {
     [RegisterPower]
-    public sealed class HpLossReductionPower : PowerModel
+    public sealed class HpLossReductionPower : WineFoxPower
     {
         public override PowerType Type => PowerType.Buff;
 
         public override PowerStackType StackType => PowerStackType.Counter;
+        
+        public override PowerAssetProfile AssetProfile => Icons(Const.Paths.EnchantedGoldenApple);
 
         public override decimal ModifyHpLostAfterOsty(Creature target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource)
         {
