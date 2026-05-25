685c59dbd8585cc9a3757e6e0b8c8586aaf0e5a0
LuoTianOrange <2977277015@qq.com>
2026-05-25 19:46:10 +0800

    feat(cards): 更新卡牌[蓄魔涌动]和[三相祝福]的效果，添加对应卡面

--- Cards/Uncommon/ManaSurge.cs
+++ Cards/Uncommon/ManaSurge.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using STS2_WineFox.Character;
 using STS2_WineFox.Powers;
+using STS2RitsuLib.Cards.DynamicVars;
 using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
 
@@ -16,7 +17,12 @@ namespace STS2_WineFox.Cards.Uncommon
     {
         protected override IEnumerable<DynamicVar> CanonicalVars =>
         [
-            WineFoxCardVarFactory.PowerAmountVar<ChantPower>(3m),
+            ModCardVars.Computed("ChantPower", 2m, card =>
+            {
+                var raw = WineFoxCardVarFactory.ChantScaledAmount(card, "ChantPower");
+                var cap = card?.IsUpgraded == true ? 6m : 4m;
+                return Math.Min(cap, raw);
+            }),
             new CardsVar(2),
         ];
 
@@ -25,6 +31,8 @@ namespace STS2_WineFox.Cards.Uncommon
 
         [Obsolete]
         protected override IEnumerable<string> RegisteredKeywordIds => [WineFoxKeywords.Magic];
+        
+        public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
         public override CardAssetProfile AssetProfile => Art(Const.Paths.CardManaSurge);
 
@@ -34,14 +42,16 @@ namespace STS2_WineFox.Cards.Uncommon
         {
             var owner = Owner;
             var creature = owner.Creature;
+            var chantGain = WineFoxCardVarFactory.ChantScaledAmount(this, "ChantPower");
+            var cap = IsUpgraded ? 6m : 4m;
+            chantGain = Math.Min(cap, chantGain);
 
-            await PowerCmd.Apply<ChantPower>(choiceContext, creature, DynamicVars["ChantPower"].BaseValue, creature, this);
+            await PowerCmd.Apply<ChantPower>(choiceContext, creature, chantGain, creature, this);
             await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, owner);
         }
 
         protected override void OnUpgrade()
         {
-            DynamicVars["ChantPower"].UpgradeValueBy(1m);
         }
     }
 }
